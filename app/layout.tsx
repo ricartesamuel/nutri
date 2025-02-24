646df98b710caf3afc62e67100ffbbd5c569d27e
Samuel <ricarte.samuelz@gmail.com>
2025-02-23 22:11:02 -0300

    feat: adicionado logo do projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Nutri",
+  description: "Crie e edite tabelas nutricionais de forma rápida e intuitiva.",
+  generator: "v0.dev",
+  icons: {
+    icon: "/nutri-icon.jpg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
